816b4599041760536d8f83ac7cf0341ab2dd3fe3
Thomas Nieradka <213204195+tjnieradka@users.noreply.github.com>
2025-12-15 08:58:10 -0800

    Add files via upload

--- sql/01-create-database.sql
+++ sql/01-create-database.sql
@@ -1,1 +1,13 @@
+/* ============================================================
+   Create ComputerInventory database
+   ============================================================ */
 
+
+IF DB_ID('ComputerInventory') IS NULL
+BEGIN
+    CREATE DATABASE ComputerInventory;
+END;
+GO
+
+USE ComputerInventory;
+GO
