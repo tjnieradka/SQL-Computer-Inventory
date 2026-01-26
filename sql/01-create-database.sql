304402a18e537ab3b2c9635ebb14b273f3e0ece0
Thomas Nieradka <213204195+tjnieradka@users.noreply.github.com>
2026-01-26 14:11:07 -0800

    Enhance comments in create database script
    Updated comments to clarify the purpose and use cases for the database creation script.

--- sql/01-create-database.sql
+++ sql/01-create-database.sql
@@ -1,6 +1,16 @@
 /* ============================================================
-   Create ComputerInventory database
-   ============================================================ */
+   PURPOSE: Create the ComputerInventory database
+
+   USE CASES:
+   - Initial setup of the ComputerInventory lab environment
+   - Recreating the database prior to running schema scripts
+   - Learning and reference purposes
+
+   NOTES:
+   - This script creates the database only
+   - Table definitions are created separately
+   - Safe to re-run; database is created only if it does not exist
+============================================================ */
 
 
 IF DB_ID('ComputerInventory') IS NULL
@@ -10,4 +20,5 @@ END;
 GO
 
 USE ComputerInventory;
+
 GO
